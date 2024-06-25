55e5f26c4854385c716f2fb1e3f22a6e0cd3e97c
KortexDev <cacortesl01@gmail.com>
2024-06-24 21:57:14 -0500

    Se termina maquetación y lógica para el rectángulo

--- Ejercicio1/main.js
+++ Ejercicio1/main.js
@@ -2,20 +2,37 @@ const elementId = (id) => document.getElementById(id);
 
 /*BOTONES*/
 const btnTriangulo = elementId("form-caja1__button");
+const btnRectangulo = elementId("form-caja2__button");
 
 /*EVENTOS*/
 btnTriangulo.addEventListener("click", calcularTriangulo);
+btnRectangulo.addEventListener("click", calcularRectangulo);
 
 function calcularTriangulo() {
   /*FORMUARIOS */
   const formTriangulo = elementId("form-caja1");
+  const resultPerimetro = elementId("perimetro-tri");
+  const resultArea = elementId("area-tri");
 
   const form = new FormData(formTriangulo);
-  const base = form.get("base");
-  const ladoA = form.get("lado-a");
-  const ladoB = form.get("lado-b");
-  const altura = form.get("altura");
+  const base = parseFloat(form.get("base"));
+  const ladoA = parseFloat(form.get("lado-a"));
+  const ladoB = parseFloat(form.get("lado-b"));
+  const altura = parseFloat(form.get("altura"));
 
-  console.log(base, ladoA, ladoB, altura);
-  console.log(inputTriBase, inputTriLadoA, inputTriLadoB, inputTriAltura);
+  resultPerimetro.textContent = `${ladoA + ladoB + base} metros`;
+  resultArea.textContent = `${(base * altura) / 2} metros cuadrados`;
+}
+function calcularRectangulo() {
+  /*FORMUARIOS */
+  const formRectangulo = elementId("form-caja2");
+  const resultPerimetro = elementId("perimetro-rect");
+  const resultArea = elementId("area-rect");
+
+  const form = new FormData(formRectangulo);
+  const base = parseFloat(form.get("base"));
+  const altura = parseFloat(form.get("altura"));
+
+  resultPerimetro.textContent = `${(altura + base) * 2} metros`;
+  resultArea.textContent = `${base * altura} metros cuadrados`;
 }
